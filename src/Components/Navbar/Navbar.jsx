15f4e6b026d755ff1f166eb8d3fdd7b610d618dc
Jamison-Lii <ntuvlii5@gmail.com>
2025-02-15 01:35:31 +0100

    More responsiveness added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Navbar.css';
-import menu from '../Assets/icon-menu.svg';
+import menu1 from '../Assets/icon-menu.svg';
 import close from '../Assets/icon-menu-close.svg';
 import logo from  '../Assets/logo1.png';
 import { Link } from 'react-router-dom';
@@ -12,6 +12,8 @@ const Navbar = () => {
     setShowMenu(prev => !prev); // ✅ Correctly toggling state
   }
 
+  const [menu, setMenu] = useState("Home");
+
   return (
     <>
       <div className='navbar'>
@@ -19,13 +21,28 @@ const Navbar = () => {
           <img className="logo" src={logo} alt="logo" />
           <p className='logo-text'>GroupBuy</p>
         </div>
-
+      <div className="navlinks-section">
         <div className='menu'>
           {/* ✅ Pass function reference, not function call */}
-          <img className='menu_icon' onClick={handleToggle} src={menu} alt="Menu Icon" />
+          <img className='menu_icon' onClick={handleToggle} src={menu1} alt="Menu Icon" />
+
+          <ul className='list-items-lg '>
+            
+            <li onClick={() => {setMenu("Home")}} style={{listStyle: 'none'}}> <Link style={{textDecoration: 'none'}} to='/'>Home</Link> {menu==="Home"?<hr/>:<></>}</li>
+            
+            <li onClick={() => {setMenu("Products")}}> <Link style={{textDecoration: 'none'}} to='/products'>Products</Link>{menu==="Products"?<hr/>:<></>} </li>
+           
+            <li onClick={() => {setMenu("Campaigns")}}> <Link style={{textDecoration: 'none'}} to='/campaigns'>Campaigns</Link>{menu==="Campaigns"?<hr/>:<></>} </li>
+           
+            <li onClick={() => {setMenu("Orders")}}> <Link style={{textDecoration: 'none'}} to='/order'>Order</Link> {menu==="Orders"?<hr/>:<></>}  </li>
+            
+            <li onClick={() => {setMenu("Profile")}}> <Link style={{textDecoration: 'none'}} to='/profile'>Profile</Link> {menu==="Profile"?<hr/>:<></>}</li>
+           
+            
+          </ul>
          
+          </div>
         </div>
-        
       </div>
       <hr className='line' />
       {showMenu ? showMenu && (
@@ -50,7 +67,6 @@ const Navbar = () => {
           )
           :
           <>
-          
           </>
           
           }
