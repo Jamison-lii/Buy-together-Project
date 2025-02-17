15ae036617ea3532304aef9b5ee950dd02e62171
Jamison-Lii <ntuvlii5@gmail.com>
2025-02-17 22:26:16 +0100

    Part of first Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './Styles/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className="openMessage">
+        Join Group Buying Campaigns  -Buy More Pay less
+       
+      </div>
+      <div className="homeContent">
+        <h1> Get massive discounts by Joining Group Buying Campaigns.</h1>
+        
+      </div> 
+
+      <button className='seeCampaigns-btn'>See Campaigns</button>
+    </div>
   )
 }
 
